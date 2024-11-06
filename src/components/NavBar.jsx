1a1eb5fc8b76af12a59fc56e30c9514cb4676e3e
jborruel308@me.com <jborruel308@me.com>
2024-11-05 19:34:18 -0800

    components setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link></Link></li>
+                <li><Link></Link></li>
+            </ul>
+        </nav>
+    )
+}
