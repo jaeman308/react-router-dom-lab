c80b70460952d13015a3c32d2d4fcd3508b8deeb
jborruel308@me.com <jborruel308@me.com>
2024-11-07 12:58:52 -0800

    complete form setup

--- src/components/MailboxForm.jsx
+++ src/components/MailboxForm.jsx
@@ -1,6 +1,34 @@
 const MailboxForm = () => {
+
+    // const [newMailbox, setNewMailbox] = useState ({
+    //     boxholder: '',
+    //     size: '',
+    // });
+
     return (
+        <>
         <h1>New Mailbox</h1>
+        <form>
+            <label htmlFor="boxholder">Enter a Boxholder</label>
+            <input
+            id= "boxholder"
+            name= "boxholder"
+            // value={newMailbox.boxholder}
+            />
+            <br />
+            <label htmlFor="boxsize">Select a Box Size</label>
+            <select
+            id= "boxsize">
+
+            <option value="">Select</option>
+            <option value="small">small</option>
+            <option value="medium">medium</option>
+            <option value="large">large</option>
+            <option value="xlarge">xlarge</option>
+            </select>
+        </form>
+        <button>Submit</button>
+        </>
     )
 }
 
