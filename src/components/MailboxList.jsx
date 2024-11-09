7676aadc4441800f02a5ff348fb01bb14df9cda1
Edgar Lopez <edgarlopez755@gmail.com>
2024-11-09 12:52:42 -0800

    add fuction for mailboxes

--- src/components/MailboxList.jsx
+++ src/components/MailboxList.jsx
@@ -1,10 +1,22 @@
-
+// import React from 'react'
 import { Link } from 'react-router-dom'
 
 
 
-const MailboxList = ({ mailboxes }) => {
-    
+const MailboxList = (props) => {
+    return (
+      <>
+        <h1>Maibox List</h1>
+        <ul>
+            {props.mailbox.map((currentMailbox) => (
+                <li key={currentMailbox._id}>
+                    <Link className='mailboxList' to={`/maikboxes/${currentMailbox._id}`}>
+                    {`Mailbox ${currentMailbox._id}`}</Link>
+                </li>
+            ))}
+        </ul>
+      </>
+    )
 }
 
 export default MailboxList;
