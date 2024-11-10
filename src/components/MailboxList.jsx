fd1c41550e9684ef88b1952d916d6eabfa48a052
Edgar Lopez <edgarlopez755@gmail.com>
2024-11-09 16:06:25 -0800

    setup mailbox details

--- src/components/MailboxList.jsx
+++ src/components/MailboxList.jsx
@@ -2,21 +2,27 @@
 import { Link } from 'react-router-dom'
 
 
-
-const MailboxList = (props) => {
+const MailboxList = ({ mailboxes }) => {
     return (
-      <>
-        <h1>Maibox List</h1>
-        <ul>
-            {props.mailbox.map((currentMailbox) => (
-                <li key={currentMailbox._id}>
-                    <Link className='mailboxList' to={`/maikboxes/${currentMailbox._id}`}>
-                    {`Mailbox ${currentMailbox._id}`}</Link>
-                </li>
-            ))}
-        </ul>
-      </>
+        <div>
+            <h1>Mailboxes</h1>
+            <ul>
+                {mailboxes.length > 0  ? (
+                    mailboxes.map((mailbox) => (
+                        <li key={mailbox._id} className='mail-box'>
+                            <Link to={`/mailboxes/${mailbox._id}`}>
+                            Mailbox {mailbox._id}
+                            </Link>
+                        </li>
+                    ))
+                ) : (
+                    <li>No mailboxes found.</li>
+                
+                )}
+            </ul>
+        </div>
     )
 }
+
 
 export default MailboxList;
